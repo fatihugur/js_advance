94a69f11e1e213af2757984fba7fcfa901ed69d8
Fatih Ugur <fatih.ugur@appwise.be>
2021-04-19 15:58:45 +0200

    Cat class

--- main_class.js
+++ main_class.js
@@ -1,11 +1,37 @@
 
-function name(){
-	return "Fatih";
 
+//CLASS OOP- OBJECT ORIENTED PROGRAMMING
+
+// function name(){
+// 	return "Fatih ";
+
+// }
+
+// function surname(){
+// 	return  "Ugur";
+// }
+
+// console.log(name()+" "+surname());
+
+"use strict"
+
+class Cat{
+	constructor(name, color, age){
+		this.name = name;
+		this.color = color;
+		this.age = age;
+
+	}
+	colorCat(){
+		console.log(this.color);
+	}
+	nameCat(){ //class içinde function yazılmaz. / method gibi davranıyor
+		console.log(this.name);
+	}
 }
+//var date = new Date();
+var newCat =new Cat("Simon", "white", 3);
+newCat.nameCat();
+newCat.colorCat();
 
-function surname(){
-	return  "Ugur";
-}
 
-console.log(name()+" "+surname());
