9de3f89e7b65d39ef5490a8058f0f461cd571e2e
Fatih Ugur <fatih.ugur@appwise.be>
2021-04-21 10:34:45 +0200

    all js html identifiers

--- main_html.js
+++ main_html.js
@@ -1,8 +1,14 @@
 
-//HTML JS relations
+//HTML JS relations // js identifier
 var fatih1 = document.getElementById("fatih1");
 // fatih1.innerText = "Fatih5";   //innerText
 fatih1.innerHTML += "<div>Fatih7</div>";  //innerHTML
-var fatih2 = document.getElementsByClassName("fatih2")[0];  //work as array
+var fatih2 = document.getElementsByClassName("fatih2")[0];  //work as array  // getELementsByClassName
 fatih2.innerText = "Fatih3";
-console.log(document. getElementsByClassName("fatih2").length);
+console.log(document. getElementsByClassName("fatih2").length); // see 2 in console // getELementsByClassName
+
+var fatih3 = document.getElementsByName("fatih3")[0]; // getElementsByName
+fatih3.innerText = "Fatih 10";
+
+var fatih4 = document.getElementsByTagName("section")[0]; // getElementsByTagName
+fatih4.innerText += " Fatih Ugur"
