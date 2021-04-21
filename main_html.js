574186b83d9359247a856408333c862d26b47b0a
Fatih Ugur <fatih.ugur@appwise.be>
2021-04-21 10:19:32 +0200

    class usage in js

--- main_html.js
+++ main_html.js
@@ -3,3 +3,6 @@
 var fatih1 = document.getElementById("fatih1");
 // fatih1.innerText = "Fatih5";   //innerText
 fatih1.innerHTML += "<div>Fatih7</div>";  //innerHTML
+var fatih2 = document.getElementsByClassName("fatih2")[0];  //work as array
+fatih2.innerText = "Fatih3";
+console.log(document. getElementsByClassName("fatih2").length);
