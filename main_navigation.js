d424ddd1337b4c5856aaa62068f735869a0b7873
Fatih Ugur <fatih.ugur@appwise.be>
2021-04-21 15:36:35 +0200

    button functions in html js

--- main_navigation.js
+++ main_navigation.js
@@ -4,5 +4,9 @@
 // var firstChild = parent.firstChild;
 // var lastChild = parent.lastChild;
 // var children = parent.children;
-var jr = document.getElementsByClassName("firstJr")[0];
-console.log(jr.parentElement.children[0]);
+// var jr = document.getElementsByClassName("firstJr")[0];
+// console.log(jr.parentElement.children[0]);
+
+//MOUSE ACTIONS
+
+var button = document.getElementsByTagName("button");
